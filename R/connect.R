645dede1ceb06a0e4e5302935bcb922817d0b25b
Kevin Frac <kevin.frac@gmail.com>
2025-04-04 16:01:24 +0200

    Add connect()

---
+++ R/connect.R
@@ -0,0 +1,19 @@
+#' Connect to AVONET database
+#'
+#' @param username Username for AVONET database
+#' @param pw Password for AVONET database
+#'
+#' @return A database connection
+#' @export
+#'
+#' @examples
+#' con <- connect(user = "postgres", password = "Frankfurterstr25!")
+connect <- function(username, pw){
+  con <- DBI::dbConnect(RPostgres::Postgres(),
+                        dbname = "avotest",
+                        host = "localhost",
+                        port = 5432,
+                        user = username,
+                        password = pw)
+  return(con)
+}
