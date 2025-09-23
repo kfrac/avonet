36f7c582755098c0423e0b1becd104955730ada2
kfrac <kevin.frac@gmail.com>
2025-09-23 18:42:15 +0200

    Add function without args for users to discover trait groups

---
+++ R/get_trait_groups.R
@@ -0,0 +1,6 @@
+get_trait_groups <- function() {
+  vector <- c("eco_trait_species", "geo_data_species", "reproductive_trait_species", "social_trait_species")
+  names(vector) <- c("eco", "geodata", "reproductive", "social")
+
+  return(names(vector))
+}
