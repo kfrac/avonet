d7f35d5b7682d067786135e4f46ad57a2eb94ccb
kfrac <kevin.frac@gmail.com>
2025-10-24 16:09:08 +0200

    Include morpho traits and rename reproductive to reprod

--- R/get_trait_groups.R
+++ R/get_trait_groups.R
@@ -1,6 +1,6 @@
 get_trait_groups <- function() {
-  vector <- c("eco_trait_species", "geo_data_species", "reproductive_trait_species", "social_trait_species")
-  names(vector) <- c("eco", "geo", "reproductive", "social")
+  vector <- c("eco_trait_species", "geo_data_species", "reproductive_trait_species", "social_trait_species", "morph_trait_specimen")
+  names(vector) <- c("eco", "geo", "reprod", "social", "morpho")
 
   return(names(vector))
 }
--- R/get_trait_list.R
+++ R/get_trait_list.R
@@ -9,8 +9,9 @@ get_trait_list <- function(group) {
 
   trait_groups_dict[["eco"]] <- list_traits("eco_trait_species")
   trait_groups_dict[["geo"]] <- list_traits("geo_data_species")
-  # trait_groups_dict[["reproductive"]] <- list_traits("reproductive")
+  # trait_groups_dict[["reprod"]] <- list_traits("reproductive")
   # trait_groups_dict[["social"]] <- list_traits("social")
+  trait_groups_dict[["morpho"]] <- list_traits("morph_trait_specimen")
 
   trait_groups_dict[[group]]$column_name[-c(1, 2)] <- sub("^[^_]*_", "", trait_groups_dict[[group]]$column_name[-c(1, 2)])
 
