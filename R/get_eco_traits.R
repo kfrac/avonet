49f90caa08f3c87f7383fd8a2ec808cbe69dd9e8
kfrac <kevin.frac@gmail.com>
2025-10-25 13:00:43 +0200

    Create get_eco_traits function

---
+++ R/get_eco_traits.R
@@ -0,0 +1,45 @@
+get_eco_traits <- function(con, trait, value, taxonomy = 1){
+
+  prefix <- "ect_"
+
+  eco_traits <- c("habitat", "habitat_density", "migration",
+                  "trophic_level", "trophic_niche", "primary_lifestyle")
+
+  if (!trait %in% eco_traits) {
+    stop("Invalid input for trait.")
+  } else{
+    var <- paste0(prefix, trait)
+  }
+
+  # if (trait == "habitat") {
+  #   var <- paste0(prefix, trait)
+  # } else if (trait == "habitat_density") {
+  #   var <- paste0(prefix, trait)
+  # } else if (trait == "migration") {
+  #   var <- paste0(prefix, trait)
+  # } else if (trait == "trophic_level") {
+  #   var <- paste0(prefix, trait)
+  # } else if (trait == "trophic_niche"){
+  #   var <- paste0(prefix, trait)
+  # } else if (trait == "primary_lifestyle"){
+  #   var <- paste0(prefix, trait)
+  # }
+
+  tbl1 <- "species"
+  tbl2 <- "eco_trait_species"
+
+  sql <- glue_sql("
+    SELECT *
+    FROM {`tbl1`}, {`tbl2`}
+    WHERE {`tbl1`}.species_id = {`tbl2`}.species_id
+    AND {`tbl2`}.{`var`} = $1
+    AND {`tbl1`}.species_tax = $2
+  ", .con = con)
+
+
+  query <- DBI::dbSendQuery(con, sql)
+  DBI::dbBind(query, list(value, taxonomy))
+  res <- DBI::dbFetch(query)
+
+  return(res)
+}
