8f74f019ad60f5fd82de0f19af85d36980ee8865
Kevin Frac <kevin.frac@gmail.com>
2025-11-08 11:29:51 +0100

    Update function to remove column prefixes based on text not position

--- R/get_trait_list.R
+++ R/get_trait_list.R
@@ -7,13 +7,19 @@ get_trait_list <- function(group) {
 
   trait_groups_dict <- list()
 
+  table_prefixes <- c("ect_", "spd_")
+
   trait_groups_dict[["eco"]] <- list_traits("eco_trait_species")
   trait_groups_dict[["geo"]] <- list_traits("geo_data_species")
-  # trait_groups_dict[["reprod"]] <- list_traits("reproductive")
-  # trait_groups_dict[["social"]] <- list_traits("social")
+  # trait_groups_dict[["reprod"]] <- list_traits("reproductive") update table_prefixes
+  # trait_groups_dict[["social"]] <- list_traits("social") update table_prefixes
   trait_groups_dict[["morpho"]] <- list_traits("morph_trait_specimen")
 
-  trait_groups_dict[[group]]$column_name[-c(1, 2)] <- sub("^[^_]*_", "", trait_groups_dict[[group]]$column_name[-c(1, 2)])
+  new_names <- ifelse(Reduce(`|`, lapply(table_prefixes, startsWith, x = trait_groups_dict[[group]]$column_name)),
+                      sub("^[^_]*_", "", trait_groups_dict[[group]]$column_name),
+                      trait_groups_dict[[group]]$column_name)
+
+  trait_groups_dict[[group]]$column_name <- new_names
 
   return(trait_groups_dict[[group]])
 }
