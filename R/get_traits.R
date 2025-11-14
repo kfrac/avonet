0f84de3e40b133dd784dc49a2e457cd136278786
kfrac <kevin.frac@gmail.com>
2025-11-14 19:08:24 +0100

    Restructure get_traits() to output sources -- previously metadata -- and new succinct metadata overview

--- R/get_traits.R
+++ R/get_traits.R
@@ -1,14 +1,33 @@
-get_traits <- function(con, species, taxonomy) {
+get_traits <- function(con, species, taxonomy, source_cols = FALSE) {
+
+  #source_cols = FALSE
+
+  #species <- "Buteo buteo"
+
+  prefixes <- c("ect_", "spd_", "geo_", "species_")
+  suffixes <- c("id", "_src", "_source")
 
   results <- list()
 
   species_data <- sql_query(con = con, parameter1 = species, parameter2 = taxonomy)
 
-  metadata <- get_metadata(species_data = species_data)
+  src_suffixes <- c("_source", "src")
+  src_pattern <- paste0("(", paste(src_suffixes, collapse="|"), ")$")
+  src_cols <- species_data[, grep(src_pattern, names(species_data))]
 
-  results <- list("metadata" = metadata, "data" = species_data)
+  metadata <- get_metadata(src_cols = src_cols)
 
-  message(sprintf("Output contains data from %d sources. Please refer to the metadata for details.", nrow(metadata)))
+  sources <- get_sources(src_cols = src_cols)
+
+  species_data <- remove_prefixes(species_data, prefixes = prefixes)
+
+  if(source_cols == FALSE){
+    species_data <- remove_suffix_columns(species_data, suffixes = suffixes)
+  }
+
+  results <- list("metadata" = metadata, "data" = species_data, "sources" = sources)
+
+  message(sprintf("Output contains data from %d sources. Please refer to the metadata for details.", nrow(sources)))
 
   return(results)
 
