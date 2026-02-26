062a73931c1500e49e44009673624b6f4037391b
kfrac <kevin.frac@gmail.com>
2026-02-26 17:32:06 +0100

    Add workaround with Excel sheet to include short description and primary source in metadata output

--- R/get_metadata.R
+++ R/get_metadata.R
@@ -9,6 +9,19 @@ get_metadata <- function(src_cols){
 
   metadata_output <- arrange_metadata(src_cols, names(src_cols))
 
+  #### Temp fix from Excel sheet and list_traits ####
+  trait_sheet <- readxl::read_xlsx("C:/Users/kfrac/Downloads/AVONET_Traits_MS_SF_KF_JAT.xlsx")
+  trait_sheet <- trait_sheet[c("trait_name_R", "short_description_R", "primary_source_R")]
+  trait_sheet <- trait_sheet[complete.cases(trait_sheet),]
+
+  ## Join to metadata output
+  metadata_output <- left_join(metadata_output, trait_sheet, by = join_by("trait" == "trait_name_R"))
+
+  ## Rename columns
+  names(metadata_output)[names(metadata_output) == "short_description_R"] <- "description"
+  names(metadata_output)[names(metadata_output) == "primary_source_R"] <- "primary_source"
+  #### END ####
+
   return(metadata_output)
 
 }
