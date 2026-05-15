db374c4d6a93c54308dbda30718f43c8aacc45e0
kfrac <kevin.frac@gmail.com>
2026-05-15 15:08:43 +0200

    Initial documentation for get_metadata()

--- R/get_metadata.R
+++ R/get_metadata.R
@@ -1,3 +1,23 @@
+#' Retrieve metadata for an AVONET query
+#'
+#' @param src_cols Source columns
+#'
+#' @returns A dataframe
+#' @export
+#'
+#' @examples
+#' db_user <- keyring::key_list("avonet")[1, "username"]
+#' db_password <- keyring::key_get("avonet", username = db_user)
+#' con <- connect_db(username = db_user, pw = db_password)
+#'
+#' species_data <- sql_query(con = con, parameter1 = "Buteo buteo", parameter2 = 1)
+#'
+#' src_pattern  <- "(_source|src)$"
+#' src_cols     <- species_data[, grep(src_pattern, names(species_data))]
+#'
+#' metadata <- get_metadata(src_cols = src_cols)
+#'
+#' metadata
 get_metadata <- function(src_cols){
 
   prefixes <- c("ect_", "spd_", "geo_", "species_")
@@ -12,10 +32,10 @@ get_metadata <- function(src_cols){
   #### Temp fix from Excel sheet and list_traits ####
   trait_sheet <- readxl::read_xlsx("C:/Users/kfrac/Downloads/AVONET_Traits_MS_SF_KF_JAT.xlsx")
   trait_sheet <- trait_sheet[c("trait_name_R", "short_description_R", "primary_source_R")]
-  trait_sheet <- trait_sheet[complete.cases(trait_sheet),]
+  trait_sheet <- trait_sheet[stats::complete.cases(trait_sheet),]
 
   ## Join to metadata output
-  metadata_output <- left_join(metadata_output, trait_sheet, by = join_by("trait" == "trait_name_R"))
+  metadata_output <- dplyr::left_join(metadata_output, trait_sheet, by = dplyr::join_by("trait" == "trait_name_R"))
 
   ## Rename columns
   names(metadata_output)[names(metadata_output) == "short_description_R"] <- "description"
