c9b1c767b3a9447b066d0b4ddaf9341cea728fdb
Kevin Frac <kevin.frac@gmail.com>
2026-07-14 19:10:52 +0200

    Refactor and document trait_description_query()

--- R/trait_description_query.R
+++ R/trait_description_query.R
@@ -1,77 +1,24 @@
+#' Retrieve trait descriptions
+#'
+#' `trait_description_query()` retrieves the descriptions and possible values of
+#' each trait.
+#'
+#' @return A dataframe with descriptions and possible values of each trait,
+#'   specifically columns \code{trait_name}, \code{trait_description},
+#'   \code{trait_value} as well as \code{value_desc_short} and
+#'   \code{value_desc_long}.
+#' @export
+#'
+#' @examples
+#' trait_desc <- trait_description_query()
+#' trait_desc
 trait_description_query <- function() {
 
   con <- get_con()
 
-  query <- paste("
-  SELECT
-    cls.relname AS table_name,
-    att.attname AS column_name,
-    typ.typname AS data_type,
-    des.description AS data_type_comment,
-    col_des.description AS column_comment,
-    CASE
-      WHEN typ.typtype IN ('e','d','c') THEN 'user-defined'
-      ELSE 'built-in'
-    END AS type_origin
-  FROM
-    pg_class AS cls
-    JOIN pg_namespace AS ns ON ns.oid = cls.relnamespace
-    JOIN pg_attribute AS att ON att.attrelid = cls.oid
-    JOIN pg_type AS typ ON typ.oid = att.atttypid
-    LEFT JOIN pg_description AS des ON des.objoid = typ.oid AND des.classoid = 'pg_type'::regclass
-    LEFT JOIN pg_description AS col_des
-    ON col_des.objoid = att.attrelid
-    AND col_des.objsubid = att.attnum
-    AND col_des.classoid = 'pg_class'::regclass
-  WHERE
-    cls.relkind = 'r'
-  AND
-    att.attnum > 0
-    --AND ns.nspname NOT IN ('pg_catalog', 'information_schema')
-    AND cls.relname IN ('eco_trait_species', 'geo_data_species', 'reproduction_trait_species', 'social_trait_species', 'morph_trait_specimen')
-    AND att.attname NOT LIKE '%\\_id%' ESCAPE '\\'
-    AND att.attname NOT LIKE '%\\_src%' ESCAPE '\\'
-    AND att.attname NOT LIKE '%\\_source%' ESCAPE '\\'
-  ORDER BY
-    cls.relname, att.attnum;")
-
-  query <- DBI::dbSendQuery(con, query)
+  query <- DBI::dbSendQuery(con, glue::glue_sql("SELECT * FROM trait_info_detailed", .con = con))
   result <- DBI::dbFetch(query)
   DBI::dbClearResult(query)
 
-  # Use data_type_comment if it contains "[Long]",
-  # otherwise use column_comment
-  source_text <- ifelse(
-    grepl("\\[LONG\\]", result$data_type_comment),
-    result$data_type_comment,
-    result$column_comment
-  )
-  # Remove trailing newline(s) at end of string
-  source_text <- sub("\n+$", "", source_text)
-  # Replace the last newline character in each string with a space
-  source_text <- sub("\n([^\n]*)$", " \\1", source_text)
-  # Split into Long / Short columns
-  # "\\s*" allows for zero or more spaces after "[Short]:"
-  parts <- strsplit(source_text, " \\[SHORT\\]:\\s*")
-  # Create new columns
-  result[c("long_description", "short_description")] <- do.call(
-    rbind,
-    lapply(parts, function(x) {
-      c(
-        # Remove "[Long]:" plus any optional whitespace after it
-        sub("^\\[LONG\\]:\\s*", "", x[1]),
-        x[2]
-      )
-    })
-  )
-
-  names(result)[names(result) == 'column_name'] <- 'trait'
-
-  result <- result[,c("trait", "short_description", "long_description")]
-
   return(result)
 }
-# Alternative method for using SQL file directly in R
-# file <- "C:/Users/kfrac/Senckenberg Dropbox/Kevin Frac/AVONET_DB/Result Queries/Query_traits_types_meta_data.sql"
-# df <- dbGetQuery(con, statement = readr::read_file(file))
-# df
