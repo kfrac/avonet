0fafe047a62fc1599770cbbce993efd44e3d8a69
Kevin Frac <kevin.frac@gmail.com>
2026-08-20 18:51:59 +0200

    Update displayed trait groups

--- R/get_trait_groups.R
+++ R/get_trait_groups.R
@@ -3,7 +3,8 @@
 #' `get_trait_groups()` returns a character vector of the groups of traits that
 #' are available in the AVONET dataset.
 #'
-#' @returns A character vector of trait groups, e.g. "eco", "morpho", "geo"
+#' @returns A character vector of trait groups, e.g. "eco", "social", "morpho", "geo".
+#' Further trait groups, e.g. "reproductive" and "demo" will be implemented soon.
 #' @export
 #'
 #' @examples
@@ -12,13 +13,13 @@
 get_trait_groups <- function() {
   vector <- c("eco_trait_species",
               #"reproductive_trait_species",
-              #"social_trait_species",
+              "social_trait_species",
               #"demo_trait_specimen",
               "morph_trait_specimen",
               "geo_data_species")
   names(vector) <- c("eco",
-                     #"reprod",
-                     #"social",
+                     #"reproductive",
+                     "social",
                      #"demo",
                      "morpho",
                      "geo")
--- R/get_trait_list.R
+++ R/get_trait_list.R
@@ -22,12 +22,12 @@ get_trait_list <- function(group = NULL) {
 
   trait_groups_dict <- list()
 
-  trait_groups_dict[["eco"]] <- list_traits("eco_trait_species")
-  trait_groups_dict[["reproduction"]] <- list_traits("reproduction_trait_species")
-  trait_groups_dict[["social"]] <- list_traits("social_trait_species")
-  # trait_groups_dict[["demo"]] <- list_traits("demo") update table_prefixes
-  trait_groups_dict[["morpho"]] <- list_traits("morph_trait_specimen")
-  trait_groups_dict[["geo"]] <- list_traits("geo_data_species")
+  trait_groups_dict[["eco"]]          <- list_traits("eco_trait_species")
+  #trait_groups_dict[["reproductive"]] <- list_traits("reproduction_trait_species") empty table in DB
+  trait_groups_dict[["social"]]       <- list_traits("social_trait_species")
+  # trait_groups_dict[["demo"]]       <- list_traits("demo") update table_prefixes
+  trait_groups_dict[["morpho"]]       <- list_traits("morph_trait_specimen")
+  trait_groups_dict[["geo"]]          <- list_traits("geo_data_species")
 
   if(is.null(group)) {
     lapply(names(trait_groups_dict), function(nm) {
